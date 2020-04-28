8946a9d55a60f34aaff6345b1248f29b9b1d52cf
runimo <63205791+runimo@users.noreply.github.com>
2020-04-28 23:06:37 +0200

    use constants for action types

--- index.js
+++ index.js
@@ -24,13 +24,19 @@ function createStore (reducer) {
   }
 }
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 function todos (state = [], action) {
     switch(action.type) {
-        case 'ADD_TODO':
+        case ADD_TODO:
           return state.concat([action.todo])
-        case 'REMOVE_TODO':
+        case REMOVE_TODO:
           return state.filter((todo) => todo.id !== action.id )
-        case 'TOGGLE_TODO':
+        case TOGGLE_TODO:
           return state.map((todo) => todo.id !== action.id 
           ? todo 
           : Object.assign({}, todo, { complete: !todo.complete }))
@@ -41,9 +47,9 @@ function todos (state = [], action) {
 
   function goals (state = [], action) {
       switch(action.type) {
-          case 'ADD_GOAL':
+          case ADD_GOAL:
             return state.concat([action.goal])
-          case 'REMOVE_GOAL':
+          case REMOVE_GOAL:
             return state.filter((goal) => goal.id !== action.id)
           default:
             return state
@@ -63,7 +69,7 @@ function todos (state = [], action) {
   })
 
   store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
       id: 0,
       name: 'Walk the dog',
@@ -72,7 +78,7 @@ function todos (state = [], action) {
   })
   
   store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
       id: 1,
       name: 'Wash the car',
@@ -81,7 +87,7 @@ function todos (state = [], action) {
   })
   
   store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
       id: 2,
       name: 'Go to the gym',
@@ -90,17 +96,17 @@ function todos (state = [], action) {
   })
   
   store.dispatch({
-    type: 'REMOVE_TODO',
+    type: REMOVE_TODO,
     id: 1
   })
   
   store.dispatch({
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id: 0
   })
   
   store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal: {
       id: 0,
       name: 'Learn Redux'
@@ -108,7 +114,7 @@ function todos (state = [], action) {
   })
   
   store.dispatch({
-    type: 'ADD_GOAL',
+    type: ADD_GOAL,
     goal: {
       id: 1,
       name: 'Lose 20 pounds'
@@ -116,6 +122,6 @@ function todos (state = [], action) {
   })
   
   store.dispatch({
-    type: 'REMOVE_GOAL',
+    type: REMOVE_GOAL,
     id: 0
   })
